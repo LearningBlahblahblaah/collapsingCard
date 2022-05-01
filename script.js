5abba62f0e58e0156c2e08793079f1d1700a59c0
Blahblahblaah <blahblahblaah13@gmail.com>
2022-05-01 21:15:15 +0800

    after tutorial guidance

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // On button click, change class to .active, and expands in position
 
+//(1) getting from DOM
 const cardList = document.querySelectorAll(".card");
-console.log("cardList", cardList);
+console.log("cardList", cardList); //returns an array
+console.log("cardList[0]", cardList[0]);
 
-const cardListKeys= Object.keys(cardList);
-
-cardListKeys.map(key => {
-    cardList[key].addEventListener("click", () => {
-        const cardActive = document.querySelector(".active");
-        cardActive.classList.remove("active");
-        cardList[key].classList.add("active");
-    });
+//(2) Add EventListener 
+cardList.forEach(card => {
+    card.addEventListener("click", () => {
+        //(2.2) removeActiveClass
+        removeActiveClasses();
+        //(2.1) add active class
+        card.classList.add("active");
+    })
 })
+
+//(3) Defining the fn to remove active classes
+function removeActiveClasses(){
+    //M1
+    // const active = document.querySelector(".active");
+    // active.classList.remove("active")
+    //M2
+    cardList.forEach(card => {
+        card.classList.remove("active");
+    })
+}
